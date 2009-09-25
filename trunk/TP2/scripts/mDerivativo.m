b23f3ce4a2ee155807f420155005a8999852ff46
santiagoshark <santiagoshark@699e62fe-9e40-11de-9fc9-ed21ab4c1028>
2009-09-25 19:53:06 +0000

    Modelo con controlador Derivativo

--- trunk/TP2/scripts/mDerivativo.m
+++ trunk/TP2/scripts/mDerivativo.m
@@ -3,8 +3,8 @@
 %x2 es tita punto
 %Valores de las constantes
 b   = 0.02;
-K   = 5;
 I   = 0.004;
+
 %Paso
 h   = 0.01;
 
@@ -13,7 +13,7 @@ t = 0 : h : 350;
 
 
 %Armado de vectores
-K= 0.0001;
+kd=0.001;
 
 x1uno     = zeros(size(t));
 x2     = zeros(size(t));
@@ -27,26 +27,27 @@ for q = 1 : n-1
     %Calculo de x1(q+1)
     k11=h*x2(q);
 
-    k21= h*((-b*x2(q)+K*(0.01*(t(q))-x1uno(q)))/I);
+    k21= h*((kd*(0.01)-(b+kd)*x2(q))/I);
     k12=h*(x2(q)+(1/2)*k21);
 
-    k22=h*((-b*(x2(q)+k21/2)+K*(0.01*(t(q)+h/2)-(x1uno(q)+k11/2)))/I);
+    k22=h*((kd*(0.01)-(b+kd)*(x2(q)+(k21/2)))/I);
     k13=h*(x2(q)+1/2*k22);
-    k23=h*((-b*(x2(q)+k22/2)+K*(0.01*(t(q)+h/2)-(x1uno(q)+k12/2)))/I);
+    k23=h*((kd*(0.01)-(b+kd)*(x2(q)+k22/2))/I);
     k14=h*(x2(q)+k23);
     x1uno(q+1)= x1uno(q)+(1/6)*(k11+2*k12+2*k13+k14);
 
 
-    k24=h*((-b*(x2(q)+k23/2)+K*(0.01*(t(q)+h)-(x1uno(q)+k13/2)))/I);
+    k24=h*((kd*(0.01)-(b+kd)*(x2(q)+k23))/I);
     %Calculo de x2(q+1)
     x2(q+1)=x2(q)+(1/6)*(k21+2*k22+2*k23+k24);
 end
 
 
 %ffffffffffffffffffffffff
-K=0.001;
+%Armado de vectores
+kd=0.01;
 
-x1dos     = zeros(size(t));
+x1dos  = zeros(size(t));
 x2     = zeros(size(t));
 
 %Condiciones inicales
@@ -58,30 +59,30 @@ for q = 1 : n-1
     %Calculo de x1(q+1)
     k11=h*x2(q);
 
-    k21= h*((-b*x2(q)+K*(0.01*(t(q))-x1dos(q)))/I);
+    k21= h*((kd*(0.01)-(b+kd)*x2(q))/I);
     k12=h*(x2(q)+(1/2)*k21);
 
-    k22=h*((-b*(x2(q)+k21/2)+K*(0.01*(t(q)+h/2)-(x1dos(q)+k11/2)))/I);
+    k22=h*((kd*(0.01)-(b+kd)*(x2(q)+(k21/2)))/I);
     k13=h*(x2(q)+1/2*k22);
-    k23=h*((-b*(x2(q)+k22/2)+K*(0.01*(t(q)+h/2)-(x1dos(q)+k12/2)))/I);
+    k23=h*((kd*(0.01)-(b+kd)*(x2(q)+k22/2))/I);
     k14=h*(x2(q)+k23);
     x1dos(q+1)= x1dos(q)+(1/6)*(k11+2*k12+2*k13+k14);
 
 
-    k24=h*((-b*(x2(q)+k23/2)+K*(0.01*(t(q)+h)-(x1dos(q)+k13/2)))/I);
+    k24=h*((kd*(0.01)-(b+kd)*(x2(q)+k23))/I);
     %Calculo de x2(q+1)
     x2(q+1)=x2(q)+(1/6)*(k21+2*k22+2*k23+k24);
 end
 
-
 %fffffffffffffffff
-K=0.01;
+%Armado de vectores
+kd=1;
 
-x1tres     = zeros(size(t));
+x1tres = zeros(size(t));
 x2     = zeros(size(t));
 
 %Condiciones inicales
-x1tres(1)     = 0;
+x1tres(1)  = 0;
 x2(1)     = 0;
 
 n=length(t);
@@ -89,20 +90,21 @@ for q = 1 : n-1
     %Calculo de x1(q+1)
     k11=h*x2(q);
 
-    k21= h*((-b*x2(q)+K*(0.01*(t(q))-x1tres(q)))/I);
+    k21= h*((kd*(0.01)-(b+kd)*x2(q))/I);
     k12=h*(x2(q)+(1/2)*k21);
 
-    k22=h*((-b*(x2(q)+k21/2)+K*(0.01*(t(q)+h/2)-(x1tres(q)+k11/2)))/I);
+    k22=h*((kd*(0.01)-(b+kd)*(x2(q)+(k21/2)))/I);
     k13=h*(x2(q)+1/2*k22);
-    k23=h*((-b*(x2(q)+k22/2)+K*(0.01*(t(q)+h/2)-(x1tres(q)+k12/2)))/I);
+    k23=h*((kd*(0.01)-(b+kd)*(x2(q)+k22/2))/I);
     k14=h*(x2(q)+k23);
     x1tres(q+1)= x1tres(q)+(1/6)*(k11+2*k12+2*k13+k14);
 
 
-    k24=h*((-b*(x2(q)+k23/2)+K*(0.01*(t(q)+h)-(x1tres(q)+k13/2)))/I);
+    k24=h*((kd*(0.01)-(b+kd)*(x2(q)+k23))/I);
     %Calculo de x2(q+1)
     x2(q+1)=x2(q)+(1/6)*(k21+2*k22+2*k23+k24);
 end
+
 hold on;
 
 plot(t,x1uno,'b-');
@@ -110,14 +112,12 @@ plot(t,x1uno,'b-');
 
 plot(t,x1dos,'b-');
 
-
 plot(t,x1tres,'b-');
 
+plot(t,0.01*t,'b-');
 
-aux=t*0.01;
-plot(t,aux,'b-');
-legend('K=0.0001','K=0.001','K=0.01','Blanco');
+legend('Kd=0.001','Kd=0.01','Kd=1.00','Blanco');
 
 xlabel('Tiempo');
 ylabel('Angulo');
-print -deps mProporcional.eps
+print -deps mDerivativo.eps
