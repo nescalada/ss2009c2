aac36d5c366d53358c8c33bc843a82205fc63865
santiagoshark <santiagoshark@699e62fe-9e40-11de-9fc9-ed21ab4c1028>
2009-09-26 15:25:42 +0000

    Saco unos graficos

--- TP2/scripts/mIntegral.m
+++ TP2/scripts/mIntegral.m
@@ -1,6 +1,7 @@
 %Computadora 1
-%x1 es tita
-%x2 es tita punto
+%x1 es la integral de tita
+%x2 es tita 
+%x3 es tita punto
 %Valores de las constantes
 b   = 0.02;
 I   = 0.004;
@@ -9,7 +10,42 @@ I   = 0.004;
 h   = 0.01;
 
 %Vector de tiempo
-t = 0 : h : 350;
+t = 0 : h : 95;
+
+
+%otro mas
+%Armado de vectores
+ki=0.00001;
+
+%Condiciones inicales
+x1(1)         = 0;
+x2cuatro(1)     = 0;
+x3(1)         = 0;
+
+n=length(t);
+for q = 1 : n-1
+    %Calculo de x1(q+1)
+    k11=h*(x2cuatro(q));
+    k21=h*(x3(q));
+    k31=h*((-b*x3(q)+ki*0.01*((t(q))^2/2)-ki*x1(q))/I);
+
+    k12=h*(x2cuatro(q)+k21/2);
+    k22=h*(x3(q)+k31/2);
+    k32=h*((-b*(x3(q)+k31/2)+ki*0.01*((t(q)+h/2)^2/2)-ki*(x1(q)+k11/2))/I);
+    
+    k13=h*(x2cuatro(q)+k22/2);
+    k23=h*(x3(q)+k32/2);
+    k33=h*((-b*(x3(q)+k32/2)+ki*0.01*((t(q)+h/2)^2/2)-ki*(x1(q)+k12/2))/I);
+    
+    k14=h*(x2cuatro(q)+k23);
+    k24=h*(x3(q)+k33);
+    k34=h*((-b*(x3(q)+k33)+ki*0.01*((t(q)+h)^2/2)-ki*(x1(q)+k13))/I);
+
+    x1(q+1)= x1(q)+(1/6)*(k11+2*k12+2*k13+k14);
+    x2cuatro(q+1)=x2cuatro(q)+(1/6)*(k21+2*k22+2*k23+k24);
+    x3(q+1)=x3(q)+(1/6)*(k31+2*k32+2*k33+k34);
+end
+
 
 
 %Armado de vectores
@@ -112,17 +148,20 @@ end
 
 hold on;
 
+axis([0,95,0,2.3]);
+
+plot(t,x2cuatro,'b-');
+
 plot(t,x2uno,'b-');
 
+plot(t,x2dos,'b-');
 
-%plot(t,x2dos,'b-');
-
-%plot(t,x2tres,'b-');
+plot(t,x2tres,'b-');
 
 plot(t,0.01*t,'b-');
 
-legend('Kd=0.0001','Kd=0.001','Kd=0.01','Blanco');
+legend('ki=0.00001','ki=0.0001','ki=0.001','ki=0.01','Blanco');
 
-xlabel('Tiempo');
-ylabel('Angulo');
+xlabel('Tiempo(segundos)');
+ylabel('Angulo(radianes)');
 print -deps mIntegral.eps
